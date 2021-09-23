d3046c25a5aeeac0e2995ce5328f3ab4c33b0eab
imeneghazi <90001259+imeneghazi@users.noreply.github.com>
2021-09-23 13:48:00 -0400

    exo 1

--- exercice.py
+++ exercice.py
@@ -7,8 +7,16 @@ def get_bill(name, data):
 	INDEX_NAME = 0
 	INDEX_QUANTITY = 1
 	INDEX_PRICE = 2
-
-	return ""
+	sous_total = 0
+	for produit in data:
+		sous_total += produit[1]*produit[2]
+	taxes = sous_total*0.15
+	total = sous_total+taxes
+	affichage_nom = "{} \n".format(name)
+	affichage_sous_total = "SOUS TOTAL {:>10.2f} $\n".format(sous_total)
+	affichage_taxes = "TAXES {:>15.2f} $\n".format(taxes)
+	affichage_total = "TOTAL {:>15.2f} $\n".format(total)
+	return affichage_nom+affichage_sous_total+affichage_taxes+affichage_total
 
 
 def format_number(number, num_decimal_digits):
